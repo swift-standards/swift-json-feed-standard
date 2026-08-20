e80a3273e77b144b36b1f3ccf9b272270b77ad27
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:53:47 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 import PackageDescription
 
 extension String {
@@ -15,10 +15,10 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-json-feed-standard",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
     ],
     products: [
         .library(
